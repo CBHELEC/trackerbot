ec758650c999f7b6991e2ec3027dc93a216e91f9
CBH <149880456+CBHELEC@users.noreply.github.com>
2025-04-20 22:18:45 +0100

    Update bot.py

--- main_stuff/bot.py
+++ main_stuff/bot.py
@@ -1,24 +1,43 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import asyncio
 import warnings
+from datetime import datetime
 
 from dotenv import load_dotenv
 load_dotenv(".env")
 TOKEN2 = os.getenv('1BOT_TOKEN_2')
+TRACKER_TOKEN = os.getenv('TRACKER_OFFICIAL_TOKEN')
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(
+    command_prefix='!',
+    intents=intents,
+    application_id=1322305662973116486
+    )
+
+bot.start_time = datetime.now()
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
     print('------------------------')
-    await bot.tree.sync()
+    #await bot.tree.sync()
     await bot.change_presence(
-        activity=discord.CustomActivity(f"Finding Caches and Friends"))
+        activity=discord.CustomActivity(f"Invite Me! | {len(bot.guilds)} Servers"))
+    update_presence.start()
+        
+last_server_count = 4
+@tasks.loop(minutes=5)
+async def update_presence():
+    global last_server_count
+    current_server_count = len(bot.guilds)
+    if current_server_count != last_server_count:
+        await bot.change_presence(
+            activity=discord.CustomActivity(f"Invite Me! | {current_server_count} Servers"))
+        last_server_count = current_server_count
 
 warnings.filterwarnings('ignore') 
 bot.remove_command('help') 
@@ -26,7 +45,8 @@ bot.remove_command('help')
 async def load_extensions():
     initial_extensions = []
 
-    for filename in os.listdir('the code i need for the bot/cogs'):
+    for filename in os.listdir('main_code/cogs'):
+    #for filename in os.listdir('/home/pi/code/PublicTrackerRelease/main_code/cogs'):
         if filename.endswith('.py'):
             initial_extensions.append("cogs." + filename[:-3])
 
@@ -38,3 +58,4 @@ async def load_extensions():
 if __name__ == '__main__':
     asyncio.run(load_extensions())
     bot.run(TOKEN2)
+  #  bot.run(TRACKER_TOKEN)
